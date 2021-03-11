b93090d6f2e96b939122e5d3d2e8bacaddec1550
EricDelmonico <54674769+EricDelmonico@users.noreply.github.com>
2021-03-11 16:10:29 -0500

    Implemented necessary interfaces

---
+++ Assets/Scripts/AttackingEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface AttackingEntity : Entity
+{
+    int AttackPower { get; }
+    /// <summary>
+    /// Range in tiles
+    /// </summary>
+    int Range { get; }
+    /// <summary>
+    /// Projectiles per second
+    /// </summary>
+    float RateOfFire { get; }
+    /// <summary>
+    /// What this entity is shooting at
+    /// </summary>
+    GameObject Target { get; }
+
+    /// <summary>
+    /// Chooses a target based on proximity (for attacking towers/enemies) 
+    /// or priority (enemies prioritize the avatar)
+    /// </summary>
+    /// <param name="possibleTargets">List of possible targets</param>
+    void FindTarget(List<Entity> possibleTargets);
+    /// <summary>
+    /// Fires a projectile from this entity's position towards Target
+    /// </summary>
+    void FireProjectile();
+}
---
+++ Assets/Scripts/MovingEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface MovingEntity : Entity
+{
+    int Speed { get; }
+
+    /// <summary>
+    /// Moves this entity's position
+    /// </summary>
+    void Move();
+}
