130336175c5dd2b7d07c9c14a850f63c7a1b649c
jf7689 <jf7689@rit.edu>
2021-03-14 01:47:02 -0500

    Drops should work, Towers not so sure
    Hard to test if Towers works properly without enemies and projectiles. Also some unity functionality is not appearing in the Towers script.

---
+++ Assets/Scripts/Drops.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drops : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    float attractRange = 5f;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToPlayer();
+    }
+
+    private void MoveToPlayer()
+    {
+        
+        if (Vector3.Distance(player.transform.position, transform.position) <= attractRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
